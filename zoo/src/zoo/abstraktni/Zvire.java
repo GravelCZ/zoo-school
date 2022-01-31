6cda29fcedb7df809f8449488ea98a7871d0eebf
GravelCZ <jindra.vese@gmail.com>
2022-01-31 08:50:18 +0100

    Package cleanup, added infomation printing

--- zoo/src/zoo/abstraktni/Zvire.java
+++ zoo/src/zoo/abstraktni/Zvire.java
@@ -1,7 +1,9 @@
-package zoo;
+package zoo.abstraktni;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class Zvire {
 
@@ -11,6 +13,8 @@ public abstract class Zvire {
 	private String jmeno;
 	private boolean zijeVCesku;
 	
+	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM YYYY");
+	
 	public Zvire(LocalDate datumNarozeni, String jmeno, boolean zijeVCesku) {
 		this.datumNarozeni = datumNarozeni;
 		this.jmeno = jmeno;
@@ -18,7 +22,14 @@ public abstract class Zvire {
 	}
 	
 	public void vypisInformace() {
-		
+		System.out.println("Datum narození: " + datumNarozeni.format(formatter));
+		System.out.println("Počet hodin krmení za den: " + this.pocetKrmeniZaDen);
+		// rychlé předělání čísel do Stringu, miluji javu 8
+		System.out.println("Časy krmení: " + String.join(", ", hodinyKrmeni.stream().map(e -> {
+			return String.valueOf(e);
+		}).collect(Collectors.toList())));
+		System.out.println("Jméno: " + this.jmeno);
+		System.out.println("Žije v česku: " + (this.zijeVCesku ? "Ano" : "Ne"));
 	}
 
 	public List<Integer> getHodinyKrmeni() {
